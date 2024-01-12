8fdc6a9b7a1d1fd7afa8be5d054db6742e446cbd
fwin-lb4 <florian.windisch@lbs4.salzburg.at>
2024-01-12 12:44:37 +0100

    update

--- src/main/java/quest/flo/Logger.java
+++ src/main/java/quest/flo/Logger.java
@@ -8,24 +8,24 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Logger {
-    BufferedWriter writer;
+    File file;
     SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss:mm");
 
     public Logger(File file, String initialMessage) throws IOException {
-        this.writer = new BufferedWriter(new FileWriter(file, true));
+        this.file = file;
         if (initialMessage != null) {
             this.writeLine(initialMessage, new Date(), "Logger");
         }
     }
 
     public synchronized void writeLine(String msg, Date time, String origin) throws IOException {
-        this.writer.write(this.formatter.format(time) + "|" + origin + "|" + msg);
-        this.writer.newLine();
-        this.writer.flush();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(this.file, true))) {
+            writer.write(this.formatter.format(time) + "|" + origin + "|" + msg);
+            writer.newLine();
+        }
     }
 
     public void shutdown() throws IOException {
         this.writeLine("Shutting down Program!", new Date(), "Logger");
-        this.writer.close();
     }
 }
